4fd46fcddd89992ecb32a25a3b6df8f06f3ad35c
chenkjhadlinks <chenkj@hadlinks.com>
2016-03-13 20:27:09 +0800

    初始化项目.

---
+++ XiaoApple/XiaoApple/XiaoApple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoApple
+//
+//  Created by chenkaijie on 16/3/13.
+//  Copyright © 2016年 HadLinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XiaoApple/XiaoApple/XiaoApple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiaoApple
+//
+//  Created by chenkaijie on 16/3/13.
+//  Copyright © 2016年 HadLinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
